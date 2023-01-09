5ab348157591fa8577c68e48d6c83a50871083ab
Johnathan Booy <106690990+johnathan-booy@users.noreply.github.com>
2023-01-09 10:03:17 -0800

    Initial

---
+++ src/Card.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Card from "./Card";
+
+it("renders without crashing", () => {
+	render(<Card />);
+});
+
+it("matches snapshot", () => {
+	render(<Card />);
+});
